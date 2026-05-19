261313b6eb7cb8fb7dd5fc898d4e3081c04303cc
MintBloom <a.stegmann09@gmail.com>
2026-05-19 12:18:21 +0100

    cleaning up files and "merging" tasks

---
+++ MemoryGameJava.java
@@ -0,0 +1,136 @@
+// base code for student Memory Card Game assessment
+//
+//The cards are A B C D E F G H.
+// A player can pick card’s by entering row and column. For example, for the first pick 'row: 2' and on the next line 'column: 0'
+import java.time.Duration;
+import java.time.Instant;
+import java.util.*;
+
+public class MemoryGameJava {
+    private static final int SIZE = 4; // 4x4 board
+    private static final String HIDDEN = " * ";
+    private static String[][] board = new String[SIZE][SIZE];
+    private static boolean[][] revealed = new boolean[SIZE][SIZE];
+    private static int attemptCounter = 0;
+    private static int row1 = 4;
+    private static int column1 = 4;
+    private static int row2 = 4;
+    private static int column2 = 4;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Memory Pair Game (4x4 : 0,1,2,3)\n");
+        initBoard();
+        userInput();
+    }
+
+    // ===== Game setup =====
+    private static void initBoard() {
+        // assigns values to each position on the board
+        List<String> cards = new ArrayList<>();
+        for (char c = 'A'; c < 'A' + (SIZE * SIZE) / 2; c++) {
+            cards.add(" " + c + " ");
+            cards.add(" " + c + " ");
+        }
+        Collections.shuffle(cards);
+        Iterator<String> it = cards.iterator();
+        for (int r = 0; r < SIZE; r++) {
+            for (int c = 0; c < SIZE; c++) {
+                board[r][c] = it.next(); // value for each board position
+                revealed[r][c] = false; // determines whether a board position is revealed to the user or not
+            }
+        }
+    }
+    private static void printBoard() {
+        // this function creates the board with its row and col. numbers in the terminal 
+        // header
+        System.out.print("    ");
+        for (int c = 0; c < SIZE; c++) System.out.printf(" %d ", c);
+        System.out.println();
+        System.out.print("    ");
+        for (int c = 0; c < SIZE; c++) System.out.print("---");
+        System.out.println();
+
+        for (int r = 0; r < SIZE; r++) {
+            System.out.printf(" %d |", r);
+            for (int c = 0; c < SIZE; c++) {
+                System.out.print(revealed[r][c] ? board[r][c] : HIDDEN);
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public static void userInput() {
+        // this function will take the users choice of which cards to reveal
+        int matchesFound = 0; // number of card matches found by the user 
+        int totalMatchesFound = 8; // obtained via. (SIZE*SIZE/2)
+        Instant startTime = Instant.now(); // records first instance of time - when the gameplay has started
+        while (matchesFound < totalMatchesFound) {
+            // loop continues until all possible matches have been found
+            getCoords1(); //
+            getCoords2();
+            if (board[MemoryGameJava.row1][MemoryGameJava.column1].equals(board[MemoryGameJava.row2][MemoryGameJava.column2])) {
+                // if its a match
+                printBoard();
+                matchesFound++; // increase the matches found by the user by one
+                System.out.println("Match found! Keep going!");
+            }
+            else {
+                // if not a match
+                printBoard();
+                System.out.println("Not a match unfortunately, keep going!");
+                revealed[MemoryGameJava.row1][MemoryGameJava.column1] = false; // resets both spots on the board user has guessed and hides the value
+                revealed[MemoryGameJava.row2][MemoryGameJava.column2] = false;
+                row1 = 4; // each row and column the user has guessed is "reset" to an unacceptable value to prevent an infinite loop
+                row2 = 4;
+                column1 = 4;
+                column2 = 4;
+            }
+            // each valid pair of spots guessed on the board (not necessarily correct guesses) will increase the attempt counter by one
+            MemoryGameJava.attemptCounter++;
+        }
+        Instant finishTime = Instant.now(); // records second instance of time - when the the player has won and gameplay has finished
+        long timeElapsed = Duration.between(startTime, finishTime).toSeconds(); // calculates time elapsed between start and finish time
+        // game is finished if user reaches here
+        System.out.println(String.format("Congratulations You Win. You have found all the matching pairs! It took you %d attempts", MemoryGameJava.attemptCounter)); // win game statement
+        System.out.println("Time taken (seconds):");
+        System.out.println(timeElapsed);
+    }
+
+    public static void getCoords2() {
+        // will get the second card coordinate from the user 
+        while(column2 < 0 || column2 > 3 || row2 < 0 || row2 > 3 || revealed[row2][column2] == true) { // numbers chosen by user must return false for each condition
+            Scanner sc = new Scanner(System.in);
+            try {
+                printBoard();
+                System.out.println("Make sure your numbers are between 0-3 and are not currently revealed.");
+                System.out.print("Second column number: ");
+                column2 = sc.nextInt(); // user column number
+                System.out.print("Second row number: ");
+                row2 = sc.nextInt(); // user row number
+            } catch (InputMismatchException e) { // raises exception if a non-integer is submitted
+               System.out.println("\nPlease type a number from 0-3\n"); 
+            }
+        }
+        revealed[row2][column2] = true; // if user board space is valid it will be revealed
+    }
+
+    public static void getCoords1() {
+        // will get the first card coordinate from the user
+        while(column1 < 0 || column1 > 3 || row1 < 0 || row1 > 3 || revealed[row1][column1]==true) { // numbers chosen by user must return false for each condition
+            Scanner sc = new Scanner(System.in);
+            try {
+                printBoard();
+                System.out.println("Make sure your numbers are between 0-3 and are not currently revealed.");
+                System.out.print("First column number: ");
+                column1 = sc.nextInt(); // user column number
+                System.out.print("First row number: ");
+                row1 = sc.nextInt(); // user row number
+            } catch (InputMismatchException e) { // raises exception a non-integer is submitted
+               System.out.println("\nPlease type a number\n"); 
+            }
+        }
+        revealed[row1][column1] = true; // if user coordinate is valid it will be revealed
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,41 @@
+# Software Programming Project 1 - Memory Game/Snap
+MemoryGameJava.java is the file for this project and should come with this README
+## Instructions for Play
+1. You will be asked for a row and column selection which will be asked on separate lines e.g. "first row number: " and on the line below "first column number:  ".
+If your response to each of these is an integer from 0-3, that is not already revealed, the corresponding spot on the board will then be revealed. This step will repeat for your second chosen spot on the board.
+2. If both of these spots on the board are a match they will stay face up, and if not, they will both be flipped over, hidden and you will be asked to choose another spot on the board.
+This process will repeat until all matches on the board have been found.
+## Implemenation on top of Base Code
+### Overview
+The code for the game is complete with error handling, a measurement of elapsed time and an attempt counter. 
+ 
+### Measuring Elapsed Time
+The elapsed time counter is a set lines very similar to option 3.2 from "https://www.baeldung.com/java-measure-elapsed-time" webpage.
+The first "startTime" (line 68) variable records the time that variable created and so does the "finishTime" (line 93) variable. The difference between the two times is then calculated in "Duration.between(startTime, finishTime)" (line 94) and then converted to seconds.
+
+### Attempt Counter
+This has been defined as a class attribute of MemoryGameJava which is incremented by one for each attempt by the player. For this game an 'attempt' is any valid (as in it passes all validity checks; lines 103, 112, 121, 130) pair of guesses the user has made, regardless if those guesses are correct or not.
+
+### Error and Exception Handling
+Error handling has been broken into two main criteria for this project: checking the user has typed a number and not some other character via. try, catch, and that the user has typed the correct number.
+
+## Problems Encountered
+### Closing Scanner While in a Try, Catch Loop
+The greatest problem was when trying to close the scanner in the getCoords1() and getCoords2() functions (lines 119 and 101 respectively). When typing a close statement at the end of each loop a message would pop up saying “convert to try-with-resources”. This meant to put the opening scanner statement within the try, catch block and Java is supposed to automatically close the scanner like so:
+
+try (Scanner sc=new Scanner(System.in)) {
+//code
+//automatically closes scanner when the try block is finished
+}
+catch (InputMismatchException e){
+//code
+}
+
+However this completely broke my code in a way that I still do not quite understand; the code would run to take the users first input, but if that input failed any condition, the try, catch block would loop and immediately crash with a NoSuchElementException. Even when Java was able to print the value of whatever it was supposed to be evaluating, immediately after it would return a NoSuchElement Exception. This post on stackoverflow,
+https://stackoverflow.com/questions/47588704/java-using-scanner-with-try-with-resources
+was the only forum to post the exact same issue and there is a solution/explanation however it was a bit difficult to understand, in particular what exactly was going wrong.
+
+---
+
 # budget-calculator-with-ui
 Second Software Programming Project using Java
